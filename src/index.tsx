28e3380e78f76c42230bee5ed6b9c218224e4082
workuser <not.indev@gmail.com>
2020-05-14 14:04:22 +0900

    [fix] jsx-quotes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
 import { App } from './pages'
 import * as serviceWorker from './serviceWorker'
 
