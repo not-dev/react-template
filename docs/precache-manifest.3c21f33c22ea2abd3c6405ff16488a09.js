24fd74f61cdff61bca6592c98ed572136a93b0d7
not-dev <63234154+not-dev@users.noreply.github.com>
2020-08-22 03:11:22 +0000

    Deploying to master from  @ de5862ddccaff062f9e328451872c947d668768c 🚀

---
+++ docs/precache-manifest.3c21f33c22ea2abd3c6405ff16488a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fde5e1040db019a1372b41797d1a405",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c4a70e150ab862b49cb",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e6519959bbe945f2ad4",
+    "url": "./static/js/2.863bd9e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.863bd9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4a70e150ab862b49cb",
+    "url": "./static/js/main.bd1bbe06.chunk.js"
+  },
+  {
+    "revision": "8314e3554cb354590f57",
+    "url": "./static/js/runtime-main.05d1baf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
